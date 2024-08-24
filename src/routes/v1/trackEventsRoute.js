90ee367bea12df9a6dfe8066c9d3d9e9bbc60639
sidporwal9 <siddhartha@titan.email>
2024-08-25 01:29:15 +0530

    Revert "javascript tag integration"
    This reverts commit cd15fff893670686770bd5c1b56fde3d25ac152f.

--- src/routes/v1/trackEventsRoute.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { trackEvents } from "../../controllers/trackEventsController.js";
-
-const router = Router();
-
-router.post("/event", trackEvents);
-
-export default router;
--- src/utils/pathUtils.js
+++
@@ -1,7 +0,0 @@
-import { fileURLToPath } from "url";
-import path from "path";
-
-export const getDirname = (metaUrl) => {
-  const __filename = fileURLToPath(metaUrl);
-  return path.dirname(__filename);
-};
